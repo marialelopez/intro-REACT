f0fc9a9d6869f9a030b6d37833885f45d2814cca
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-10-20 14:36:32 -0500

    ejemplo de clase con react

---
+++ miPrimeraApp/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+return (
+    <div className='contenedor'>
+    <p>Made with our homemade basil pine nuts pesto sauce. Gluten free pasta available upon request</p>
+    <button>Mi boton</button>
+    </div>
+  )
+}
+
+export default App
